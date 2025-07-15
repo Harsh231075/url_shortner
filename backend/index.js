0022c70fe2f167ad6cd6dce32735052774caba2c
Harsh Singh <baghel.harsh@webseeder.tech>
2025-07-15 23:44:27 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.BASE_URL || 'http://localhost:5371',
   credentials: true
 }));
 
