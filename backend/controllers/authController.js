18147fc9a0f667184763d8c85b7b9f402c7824eb
Harsh singh baghel <harshsingh231075@gmail.com>
2025-07-16 09:40:07 +0530

    fix

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,6 +6,7 @@ import jwt from 'jsonwebtoken';
 export const registerUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+     if (!username || !email || !password) return res.status(400).json({ message: 'All fields are required' });
 
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(400).json({ message: 'User already exists' });
